921d82326d810eafb8eeaad2d61ceb50d3b5cd65
hermi <hermi@localhost>
2018-01-07 00:21:20 +0100

    initial commit

---
+++ src/de/ph/playground/TheVeryBeginning.java
@@ -0,0 +1,11 @@
+package de.ph.playground;
+
+public class TheVeryBeginning {
+
+    public static void main(String[] args) {
+
+        System.out.println("Congratulation! You've made it and I know you love it! :-)");
+
+    }
+
+}
